4bf90cc30e8cce2a38f61a88450c75f275648481
赵辉亮 <zhaohuiliang@bgyfw.com>
2025-11-06 14:27:24 +0800

    加入配置

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080/api',
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://39.104.72.229/api',
   },
   images: {
     remotePatterns: [
